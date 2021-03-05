b1174858163681b75a0cd84945aa92c4fbda461b
MrKoguch <pocmra@gmail.com>
2021-03-05 22:28:26 +0300

    [upg] Добавлен класс который ведет себя как словарь, но он не словарь, а наглый обманщик!

--- test.py
+++ test.py
@@ -1,7 +1,72 @@
-def g():
-    print("1")
-    yield 2
-    print("3")
+# 5_03
+from collections.abc import MutableMapping
+
+
+class DickLike(MutableMapping):
+    def __len__(self) -> int:
+        pass
+
+    def __iter__(self):
+        ...
+
+    def __init__(self):
+        self._data = {}
+
+    def __getitem__(self, item):
+        # v = d[]
+        return self._data[item]
+
+    def __setitem__(self, key, value):
+        # d[] = v
+        if not isinstance(key, int):
+            raise TypeError
+        elif key < 1:
+            raise ValueError
+        self._data[key] = value
+
+    def __delitem__(self, key):
+        # del o[]
+        del self._data[key]
+
+    def get(self, key, default=None, /):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __contains__(self, item):
+        try:
+            self[item]
+        except KeyError:
+            return False
+        return True
+
+    def setdefault(self, key, default=None, /):
+        try:
+            return self[key]
+        except KeyError:
+            self[key] = default
+            return default
+
+
+d = DickLike()
+d[3] = 4
+print(d._data)
+print(d.get(1))
+print(d.get(1, 2))
+print(1 in d)
+print(3 in d)
+print(d.setdefault(3))
+print(d.setdefault(2))
+# д\з ключи название файлов, в инит путь до директории, занчение содержимое файла,
+# если название файла, папка то вернуть словарь
+
+# какой-то другой день
+
+# def g():
+#     print("1")
+#     yield 2
+#     print("3")
 
 # g = iter(ol)  # он помнит что остановился
 # for n in g:  # аналогично for n in ol:
@@ -38,10 +103,11 @@ def g():
 # # print(next(x))
 # # print("stop")
 
-print(hash(-3))
-print(hash(-1))
-print(hash(0))
-print(hash(1))
-print(hash(2))
+# print(hash(-3))
+# print(hash(-1))
+# print(hash(0))
+# print(hash(1))
+# print(hash(2))
+#
+# s = {1: (2, 3, 4)}
 
-s = {1: (2, 3, 4)}
